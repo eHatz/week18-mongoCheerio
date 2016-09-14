d3a3a938a10ec0167d12c77c20ec5435837571a7
Efthemios Hatzis <ehatz01@gmail.com>
2016-09-14 12:19:32 -0400

    files structure mod

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var request = require('request'); 
 var cheerio = require('cheerio');
-var request = require('request');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 request('https://www.reddit.com/r/webdev', function(err, response, html) {
 	if (err) {
